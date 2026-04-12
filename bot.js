e8871be44ec66f517a20d9999347c428a30ae1d4
Bohdan Tsap <bohdaq@gmail.com>
2026-04-12 16:03:28 +0300

    wip

--- bot.js
+++ bot.js
@@ -116,8 +116,12 @@ client.on('messageCreate', async message => {
                 }
                 searchQuery = spotifyTrack.query;
                 trackTitle = spotifyTrack.title;
+            } else if (url.includes('youtube.com') || url.includes('youtu.be')) {
+                // Direct YouTube URL
+                searchQuery = url;
+                trackTitle = 'YouTube Video';
             } else {
-                return message.reply('Please provide a valid Spotify URL!');
+                return message.reply('Please provide a valid Spotify or YouTube URL!');
             }
 
             // Create or get player
